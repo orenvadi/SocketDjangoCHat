5743a9d3e15f709427c4858566e53e75133d2f4e
Narashi Kuga <orenvady@gmail.com>
2023-07-01 21:39:29 +0600

    initial commit

---
+++ auth/serializers.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from djoser.conf import settings as djoser_settings
+from djoser.serializers import TokenSerializer
+from rest_framework import serializers
+from stream_chat import StreamChat
+
+
+class StreamTokenSerializer(TokenSerializer):
+    stream_token = serializers.SerializerMethodField()
+
+    class Meta:
+        model = djoser_settings.TOKEN_MODEL
+        fields = ("auth_token", "stream_token")
+
+    def get_stream_token(self, obj):
+        client = StreamChat(
+            api_key=settings.STREAM_API_KEY, api_secret=settings.STREAM_API_SECRET
+        )
+        token = client.create_token(obj.user.id)
+
+        return token
